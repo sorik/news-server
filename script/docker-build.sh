e0fdabf6d9304be42f58fb58ab74bb3b6bb2853a
Sori Kang <sorikuya@gmail.com>
2015-06-17 16:55:23 +1000

    docker push when building image is successful

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-docker build -t sorik/news-server .
+echo "current build number is $BUILD_NUMBER, so the tag will be $BUILD_NUMBER."
+echo "building the image..."
+docker build sorik/news-server:$BUILD_NUMBER .
+
+echo "pushing the image..."
+docker push sorik/news-server:$BUILD_NUMBER
