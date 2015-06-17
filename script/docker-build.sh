2c147a1df306bb9d7e36b09f5caaff9f7bf377e5
Sori Kang <sorikuya@gmail.com>
2015-06-17 16:58:24 +1000

    add -t option on building docker image

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -2,7 +2,7 @@
 
 echo "current build number is $BUILD_NUMBER, so the tag will be $BUILD_NUMBER."
 echo "building the image..."
-docker build sorik/news-server:$BUILD_NUMBER .
+docker build -t sorik/news-server:$BUILD_NUMBER .
 
 echo "pushing the image..."
 docker push sorik/news-server:$BUILD_NUMBER
